a9957bfd7ce730cbfba599a92bdfd3fc698872e9
Mish <mjkobaya@gmail.com>
2015-05-17 17:59:38 -0700

    Initial Commit
    -Interface is done
    -Still need to implement features

---
+++ app/src/main/java/com/example/mkobayashi/drawingfuntutorial/DrawingView.java
@@ -0,0 +1,20 @@
+package com.example.mkobayashi.drawingfuntutorial;
+
+//import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by mkobayashi on 5/17/15.
+ */
+
+public class DrawingView extends android.view.View {
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+}
